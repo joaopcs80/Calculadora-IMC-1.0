e101d202fc7fb84b2785a99bc7b9845b347af494
Dev002 - Joao Paulo <ddev02@domtec.svr>
2024-07-23 10:36:05 -0300

    Projeto Pronto

--- bin/calculadoraimc.dart
+++ bin/calculadoraimc.dart
@@ -1,5 +1,45 @@
-import 'package:calculadoraimc/calculadoraimc.dart' as calculadoraimc;
+import 'dart:io';
+import 'package:calculadoraimc/pessoa.dart';
+import 'package:calculadoraimc/validacoes.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${calculadoraimc.calculate()}!');
+void main() {
+
+  String nome;
+  double peso;
+  double altura;
+
+  do {
+    stdout.write('Digite seu nome: ');
+    nome = stdin.readLineSync()!;
+
+    if (!validarTamanhoNome(nome)) {
+      print('O nome deve conter 3 dígitos ou mais');
+    }
+  } while (!validarTamanhoNome(nome));
+
+  do {
+    stdout.write('Digite seu peso (kg): ');
+    peso = double.parse(stdin.readLineSync()!);
+
+    if (!validarPesoPositivo(peso)) {
+      print('O Peso não pode ser igual ou menor a Zero');
+    }
+  } while (!validarPesoPositivo(peso));
+
+  do {
+    stdout.write('Digite sua altura (m): ');
+    altura = double.parse(stdin.readLineSync()!);
+
+    if (!validarAlturaPositiva(altura)) {
+      print('A Altura não pode ser igual ou menor a Zero');
+    }
+  } while (!validarAlturaPositiva(altura));
+
+  Pessoa pessoa = Pessoa(nome, peso, altura);
+  double imc = pessoa.calcularIMC();
+  String classificacao = pessoa.classificacaoIMC(imc);
+
+  print('\nNome: $nome');
+  print('IMC: ${imc.toStringAsFixed(2)}');
+  print('Classificação: $classificacao');
 }
---
+++ lib/validacoes.dart
@@ -0,0 +1,11 @@
+bool validarTamanhoNome(String nome) {
+  return nome.length >= 3;
+}
+
+bool validarPesoPositivo(double peso) {
+  return peso > 0;
+}
+
+bool validarAlturaPositiva(double altura) {
+  return altura > 0;
+}
